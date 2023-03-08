805d071177f22c4f4e10c0aaf541db0aabda542c
rami.dnd-at-069716778491 <ralrafie@yahoo.com>
2023-03-07 23:50:29 -0500

    initialized card, homePage, and sideBar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SideBarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
